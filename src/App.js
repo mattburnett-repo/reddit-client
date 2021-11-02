f857090517a820cc5074a9df1f002ffd2cc71062
Matt Burnett <mburnett@tutanota.de>
2021-11-02 16:04:13 +0100

    added stubs for search/filter/items functionality. added initial tests for each

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import Search from './components/search/Search.js';
+import Filter from './components/filter/Filter.js';
+import Items from './components/items/Items.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        logo image
       </header>
+      <Search />
+      <Filter />
+      <Items />
     </div>
   );
 }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,23 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+it('includes a logo image', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('logo image')).toBeInTheDocument();
+});
+
+it('includes a search tag', () => {
+  render(<App />);
+  expect(screen.getByText('search tag')).toBeInTheDocument();
+});
+
+it('includes a filter tag', () => {
+  render(<App />);
+  expect(screen.getByText('filter tag')).toBeInTheDocument();
+});
+
+it('includes an items tag', () => {
+  render(<App />);
+  expect(screen.getByText('items tag')).toBeInTheDocument();
 });
