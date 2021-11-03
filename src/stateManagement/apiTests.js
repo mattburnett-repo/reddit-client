c6aaf2ab6eb4004fe8bf1fd1e75de7a9d3143229
Matt Burnett <mburnett@tutanota.de>
2021-11-03 18:00:42 +0100

    added reduxjs/tookkit. started towards asynd / api dev

---
+++ src/stateManagement/apiTests.js
@@ -0,0 +1,9 @@
+// let's try api dev here ->
+// import {createStore} from '@reduxjs/toolkit';
+
+// createSlice
+
+// createAsyncThunk
+
+// extraReducers
+
--- src/stateManagement/store.js
+++ src/stateManagement/store.js
@@ -43,10 +43,14 @@
 
 // *********************************************
 // !!!!!!!
-//      you have to add "type": "module",  to package.json in order to test run this in node, if you use 'import'
-// import { createStore } from 'redux';
-//      if you use 'require', you don't need the "type: module" thing
-const { createStore } = require('redux');
+//      you have to add "type": "module",  to package.json in order to test run this in node (zB node store.js), 
+//          if you use -> 
+//              import { createStore } from 'redux';
+//          if you use 'require', you don't need the "type: module" thing
+
+// IMPORT
+import { createStore } from 'redux';
+// const { createStore } = require('redux'); // testing now from __tests__ folder, not running locally in node
 
 // // ACTION CREATORS
 const getItems = () => {
@@ -74,12 +78,37 @@ const searchItems = (searchTerm) => {
     }
 }
 
+// ASYNC FUNCTIONALITY
+//      provided by 'redux-thunk', which is also bundled in @reduxjs/toolkit
+//          -thunk is classified as a middleware
+//      redux 'applyMiddleware' function
+//          takes function as arg
+//          goes as third arg to createStore(). second arg is initialState?
+//      redux-thunk is such a popular solution for handling asynchronous logic that it is included in Redux Toolkit. 
+//      "It also exists as a standalone package, but you won’t need to install redux-thunk separately if you use Redux Toolkit. 
+//          This is because Redux Toolkit’s configureStore function, which you learned about in a previous lesson, 
+//          will apply redux-thunk to the store by default.
+//
+//          we may need to use tooklkit after all...
+//              import { createStore } from '@reduxjs/toolkit'; <- not from Redux...
+
+//      Code sort of looks like ->
+//          const getUser = (id) => {
+//              return async (dispatch, getState) => {
+//              const payload = await fetchUser(id)
+//              dispatch({type: 'users/addUser', payload: payload})
+//              }
+//          }
+
 // SUBSCRIBE HANDLERS
 // const reactToStateChange = () => {
 //     console.log('state changed again');
 // }
 
 // reducer
+//      have to account for filterTerm, itemsSlice, searchTerm
+//          so the reducer would have filterTerm and searchTerm, with whatever reducer stuff, then extraReducers for the asynce API stuff
+
 const initialState = {};
 const itemsReducer = (state = initialState, action) => {
     switch(action.type) {
@@ -102,21 +131,12 @@ const itemsReducer = (state = initialState, action) => {
     } // end switch
 }
 
+// SLICE
+//      replace reducer with slices?
+
 // adding "type": "module", to package.json makes this possible without export / module weirdness
 //      but you can instead use const { createStore } = require('redux'); look towards top of code...
 const store = createStore(itemsReducer);
-
-// let's do some tests!
-//      *** moved testing over to __tests__/Store.test.js
-
-// const unsubscribe = store.subscribe(reactToStateChange); // subscribe 'reactToStateChange' to store. it reads sort of backwards.
-
-// store.dispatch(getItems());
-// store.dispatch(getItemById(2));
-// console.log('unsubscribe here');
-// unsubscribe();
-// store.dispatch(filterItems('filter item'));
-// store.dispatch(searchItems('search term'));
 
 // SELECTORS
 //      how are these different from actions / action creators???
